70776563c2124b3028bbfa0b64687eec5a6796f8
Cristian <cheredia@kountable.com>
2019-09-21 01:54:28 -0700

    write test for helper functions

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,11 +1,17 @@
-from sql.helpers import get_row_count
+from sql.helpers import get_row_count, find_rate
 
 
-tables = ['timestamps', 'rates']
+tables = ["timestamps", "rates"]
 
 
 def test_get_row_count():
     for table in tables:
         result = get_row_count(table)
         assert isinstance(result, int)
- 
+
+
+def test_find_rate():
+    rates = {"2": (2, 4), "3": (9, 27), "125": (4, 500)}
+    for key, pair in rates.items():
+        rate = find_rate(*pair)
+        assert rate == eval(key)
